6b5faafe0bc39761c73dacfc6ca264206361488c
Ortus <contact@ortusagenceweb.fr>
2017-01-22 01:54:53 +0100

    Amélioration du StaticpagesController Déplacement d'une partie du code de StaticpagesController vers LAPInject

--- src/LAP/StaticpagesBundle/Controller/DefaultController.php
+++ src/LAP/StaticpagesBundle/Controller/DefaultController.php
@@ -34,33 +34,9 @@ class DefaultController extends Controller
 		/* Limit number of characters in title to 45 */
 		$t = $this->container->get('lap_admin.cuttxt');
         $listeLastmessagerie = $listes[0];
-		foreach($listeLastmessagerie as $key => $contact)
-		{
-			$txt = $listeLastmessagerie[$key]['titre'];
-			$txtcuted = $t->cuttext($txt);
-			$listeLastmessagerie[$key]['titre'] = ( trim(strip_tags(substr($txtcuted, 0, 45))) );
-		}
-		
-		/* Limit number of characters in title to 80 */
-		foreach($listeAllpages as $key => $pages)
-		{
-			$txt = $listeAllpages[$key]->getTexte();
-			$txtcuted = $t->cuttext($txt);
-			$listeAllpages[$key]->setTexte( trim(strip_tags(substr($txtcuted, 0, 80))) );
-		}
-		
-		/* Setting active value to 1 (true) directly from admin homepage */
-		/*if ($request->isMethod('POST')) {
-			$em = $this->getDoctrine()->getManager();
-			$article = $em->getRepository('LAPBlogBundle:Article')->find( $request->query->get('id') );
-			if( $request->query->get('act') == "active" ) { $article->setActive(1); }
-			elseif( $request->query->get('act') == "unactive" ) { $article->setActive(0); }
-			$em->flush();
-			
-			$request->getSession()->getFlashBag()->add('notice', 'Votre article a bien été modifié.');
-			
-			return $this->redirectToRoute('lap_admin_homepage');
-		}*/
+
+        $inject->limit45($listeLastmessagerie, $t);
+        $inject->limitStaticpages80($listeAllpages, $t);
 		
 		return $this->render('LAPStaticpagesBundle:Default:index.html.twig', array(
 			'name'                  => $util['name'],
